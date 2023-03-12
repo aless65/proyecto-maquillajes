58be71a9ecbd5de979de71abc87f3f800baa3f14
ChrizBBQ <103384328+ChrizBBX@users.noreply.github.com>
2023-03-11 20:41:35 -0600

    a

---
+++ Maquillaje/Maquillaje.DataAcces/Repository/DepartamentoRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Maquillaje.Entities.Entities;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Maquillaje.DataAccess.Repository
+{
+    public class DepartamentoRepository : IRepository<tbDepartamentos>
+    {
+        public int Delete(tbDepartamentos item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public tbDepartamentos find(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(tbDepartamentos item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<tbDepartamentos> List()
+        {
+            using var db = new SqlConnection(AndreasContext.ConnectionString);
+            return db.Query<tbDepartamentos>(ScriptsDataBase.UDP_Listar_Departamentos, null, commandType: CommandType.StoredProcedure);
+        }
+
+        public int Update(tbDepartamentos item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

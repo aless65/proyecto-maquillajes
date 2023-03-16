c26c33b1c9df96f6ed74241880d09da9d71a394f
ALeaxanderaz04 <achristopher231@gmail.com>
2023-03-16 15:47:01 -0600

    Repositorio de la vista de productos Creado

---
+++ Maquillaje/Maquillaje.DataAcces/Repository/VW_maqu_tbProductos_VW_Repository.cs
@@ -0,0 +1,35 @@
+﻿using Maquillaje.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maquillaje.DataAccess.Repository
+{
+    public class VW_maqu_tbProductos_VW_Repository : IRepository<VW_maqu_tbProductos_VW>
+    {
+        public int Delete(VW_maqu_tbProductos_VW item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VW_maqu_tbProductos_VW find(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(VW_maqu_tbProductos_VW item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<VW_maqu_tbProductos_VW> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(VW_maqu_tbProductos_VW item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

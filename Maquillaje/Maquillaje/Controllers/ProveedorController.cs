60b8f0497afb6df8507c00ff6c66a48ee27dc55d
ALeaxanderaz04 <achristopher231@gmail.com>
2023-03-17 09:38:31 -0600

    Proveedor Index

---
+++ Maquillaje/Maquillaje/Controllers/ProveedorController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Maquillaje.BusinessLogic.Services;
+using Maquillaje.Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maquillaje.WebUI.Controllers
+{
+    public class ProveedorController : Controller
+    {
+
+        private readonly MaquService _maquService;
+        private readonly IMapper _mapper;
+
+        public ProveedorController(MaquService maquService, IMapper mapper)
+        {
+            _maquService = maquService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("/Proveedor/Listado")]
+        public IActionResult Index()
+        {
+            var listado = _maquService.ListadoProveedores();
+            var listadoMapeado = _mapper.Map<IEnumerable<VW_maqu_tbProveedores_VW>>(listado);
+
+            return View(listadoMapeado);
+        }
+
+    }
+}
